72e5671edaa65f3d3e99c1a1e85238b5560f8764
john <johnnelsanhartono@gmail.com>
2026-08-28 23:30:19 -0700

    added face dection model through someone git so goated

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
 import cv2 
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+faceModel = YOLO('yolov8m-face.pt')
 
 photo = cv2.imread('photo1.jpg')
-cv2.imshow('my window', photo)
+result = model(photo)
+faceResult = faceModel(photo)
+
+personBody = result[0].plot()
+# Using the plot from the person body to also show how person face is working
+personFace = faceResult[0].plot(img=personBody)
+
+
+cv2.imshow('my window', personFace)
 cv2.moveWindow('my window', 100, 100)
 
 cv2.waitKey(5000)
